6b014edd28ca9dc91e2db1d874fc286a3f554d2a
Vrushabh Motghare <56147944+vrushabhmotghare@users.noreply.github.com>
2025-05-23 17:45:11 +0000

    Added rev_array.c file to array directory

---
+++ array/rev_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    int arr1[5];
+
+    printf("Enter the elements of the array\n");
+    for (int i=0; i < 5; i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
+    for ( int i=0; i < 5; i++){
+        printf("Elements of the array arr1[%d] = %d\n",i , arr1[i]);
+    }
+    for (int i =4 ; i>= 0; i--){
+        printf("Elements of the array in the reverse order arr1[%d] = %d\n",i , arr1[i]);
+    }
+    
+}
